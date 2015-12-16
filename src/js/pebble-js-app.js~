6a910eb56714b36fa386100b5d7e46420dc02bd3
Paul Nettleton <paulnett7@github.com>
2015-12-16 13:34:35 -0600

    Initial commit

---
+++ src/js/pebble-js-app.js~
@@ -0,0 +1,54 @@
+var xhrRequest = function (url, type, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.onload = function () {
+		callback(this.responseText);
+	};
+	xhr.open(type, url);
+	xhr.send();
+};
+
+function getNotebooks() {
+	//get OAuth token
+	
+	//send request
+}
+
+Pebble.addEventListener('ready',
+	function(e) {
+		console.log('PebbleKit JS ready');
+		
+		getNotebooks();
+	}
+);
+
+Pebble.addEventListener('appmessage',
+	function(e) {
+		console.log('AppMessage received');
+		
+		getNotebooks();
+	}
+);
+
+Pebble.addEventListener('showConfiguration', 
+	function(e) {
+		Pebble.openURL('https://pblNote.com/config-page.html');
+	}
+);
+
+Pebble.addEventListener('webviewClosed',
+	function(data) {
+		var config_data = JSON.parse(decodeURIComponent(data.response));
+		console.log('Config window returned: ', JSON.stringify(config_data));
+
+		//dictionary to send via AppMessage
+		var dict = {
+			//keys of the config data, OAuth token
+		};
+	
+		Pebble.sendAppMessage(dict, function() {
+			console.log('Sent config data to Pebble');
+		}, function() {
+			console.log('Failed to send config data!');
+		});
+	}
+);
---
+++ src/pblNote.c
@@ -0,0 +1,55 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+
+static void main_window_load(Window *window) {
+	Layer *window_layer = window_get_root_layer(window);
+}
+
+static void main_window_unload(Window *window) {
+	
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+	//deal with recieving data
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+	APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+	APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+	APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+static void init() {
+	s_main_window = window_create();
+	window_set_window_handlers(s_main_window, (WindowHandlers) {
+		.load = main_window_load,
+		.unload = main_window_unload,
+	});
+
+	window_stack_push(s_main_window, true);
+
+	//register callbacks
+	app_message_register_inbox_received(inbox_received_callback);
+	app_message_register_inbox_dropped(inbox_dropped_callback);
+	app_message_register_outbox_failed(outbox_failed_callback);
+	app_message_register_outbox_sent(outbox_sent_callback);
+
+	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+}
+
+static void deinit() {
+	window_destroy(s_main_window);
+}
+
+int main(void) {
+	init();
+	app_event_loop();
+	deinit();
+}
